64a125b8ecf6dd12a7c4a1b83aadf263d937fa4a
Jay <jaycadla@gmail.com>
2026-01-13 10:02:39 -1000

    chore: fix database config formatting

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -51,27 +51,27 @@ class Database extends Config
     //     ],
     // ];
 
-        /**
-         * Sample database connection for SQLite3.
-         *
-         * @var array<string, mixed>
-         */
-        public array $default = [
-            'database'    => 'database.db',
-            'DBDriver'    => 'SQLite3',
-            'DBPrefix'    => '',
-            'DBDebug'     => true,
-            'swapPre'     => '',
-            'failover'    => [],
-            'foreignKeys' => true,
-            'busyTimeout' => 1000,
-            'synchronous' => null,
-            'dateFormat'  => [
-                'date'     => 'Y-m-d',
-                'datetime' => 'Y-m-d H:i:s',
-                'time'     => 'H:i:s',
-            ],
-        ];
+    /**
+     * Sample database connection for SQLite3.
+     *
+     * @var array<string, mixed>
+     */
+    public array $default = [
+        'database'    => 'database.db',
+        'DBDriver'    => 'SQLite3',
+        'DBPrefix'    => '',
+        'DBDebug'     => true,
+        'swapPre'     => '',
+        'failover'    => [],
+        'foreignKeys' => true,
+        'busyTimeout' => 1000,
+        'synchronous' => null,
+        'dateFormat'  => [
+            'date'     => 'Y-m-d',
+            'datetime' => 'Y-m-d H:i:s',
+            'time'     => 'H:i:s',
+        ],
+    ];
 
     //    /**
     //     * Sample database connection for Postgre.
